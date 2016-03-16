7a5b3e16b3f8c4858ddd2121f04610c948cafbde
Igor Kozlovsky <igor@linnovate.net>
2016-03-16 19:48:53 +0300

    Move settings to DB. Drupal 7. Individual inits. Move texts to external file.

---
+++ texts.php
@@ -0,0 +1,35 @@
+<?php
+
+define('stacksight_logs_title', 'Include Logs');
+$logs_text = <<<HTML
+    <div>Some HTML description for Stacksight Logs</div>
+HTML;
+define('stacksight_logs_text', $logs_text);
+
+
+define('stacksight_health_title', 'Include Health');
+$health_text = <<<HTML
+    <div>Some HTML description for Stacksight Health</div>
+HTML;
+define('stacksight_health_text', $health_text);
+
+
+define('stacksight_inventory_title', 'Include Inventory');
+$inventory_text = <<<HTML
+    <div>Some HTML description for Stacksight Inventory</div>
+HTML;
+define('stacksight_inventory_text', $inventory_text);
+
+
+define('stacksight_events_title', 'Include Events');
+$events_text = <<<HTML
+    <div>Some HTML description for Stacksight Events</div>
+HTML;
+define('stacksight_events_text', $events_text);
+
+
+define('stacksight_updates_title', 'Include Updates');
+$updates_text = <<<HTML
+    <div>Some HTML description for Stacksight Updates</div>
+HTML;
+define('stacksight_updates_text', $updates_text);
