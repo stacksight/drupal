e91ac4bb8a2c2431bfb900749a0e838b27320613
Igor Kozlovsky <igor@linnovate.net>
2016-03-20 17:20:46 +0300

    Texts for Drupal7

--- texts.php
+++ texts.php
@@ -2,34 +2,34 @@
 
 define('stacksight_logs_title', 'Include Logs');
 $logs_text = <<<HTML
-    <div>Some HTML description for Stacksight Logs</div>
+    <div>Real time logging information of warnings and errors</div>
 HTML;
 define('stacksight_logs_text', $logs_text);
 
 
 define('stacksight_health_title', 'Include Health');
 $health_text = <<<HTML
-    <div>Some HTML description for Stacksight Health</div>
+    <div>Build a complete health profile</div>
 HTML;
 define('stacksight_health_text', $health_text);
 
 
 define('stacksight_inventory_title', 'Include Inventory');
 $inventory_text = <<<HTML
-    <div>Some HTML description for Stacksight Inventory</div>
+    <div>Collects the list of plugins, modules and packagesy</div>
 HTML;
 define('stacksight_inventory_text', $inventory_text);
 
 
 define('stacksight_events_title', 'Include Events');
 $events_text = <<<HTML
-    <div>Some HTML description for Stacksight Events</div>
+    <div>Watch users and application events at real time</div>
 HTML;
 define('stacksight_events_text', $events_text);
 
 
 define('stacksight_updates_title', 'Include Updates');
 $updates_text = <<<HTML
-    <div>Some HTML description for Stacksight Updates</div>
+    <div>Show aviliabilty of new software updates</div>
 HTML;
 define('stacksight_updates_text', $updates_text);
