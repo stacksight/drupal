8304bfb418f94bf8cb44e224dd399459a9e1305c
Igor Kozlovsky <igor@linnovate.net>
2016-03-22 20:06:22 +0300

    Fix instruction

--- code_config.tpl.php
+++ code_config.tpl.php
@@ -16,7 +16,6 @@ if (!empty($data['data']['diagnostic'])): ?>
 <p><?php echo t("Insert this configuration code (start-end block) at the end of your settings.php") ?>:</p>
 <pre class="code-ss-inlcude">
 <span class="code-comments">// StackSight start config</span>
-<span class="code-red">define</span>(<span class="code-yellow">'DRUPAL_ROOT'</span>, <span class="code-yellow">getcwd()</span>);
 <span class="code-red">define</span>(<span class="code-yellow">'STACKSIGHT_APP_ID'</span>, <span class="pre-code-red">'<?php echo $data['data']['_id'] ?>'</span>);
 <span class="code-red">define</span>(<span class="code-yellow">'STACKSIGHT_TOKEN'</span>, <span class="pre-code-red">'<?php echo $data['data']['token'] ?>'</span>);
 <span class="code-red">require_once</span>(<span class="code-blue">DRUPAL_ROOT</span> . <span class="pre-code-red">'/<?php echo $data['data']['module_path']; ?>'</span> . <span class="code-yellow">'/stacksight-php-sdk/bootstrap-drupal-6.php'</span>);
